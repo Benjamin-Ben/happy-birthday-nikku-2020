adf519c02a4c81404d15a96efab16c4bfe517bfe
Benjamin-Ben <42369601+Benjamin-Ben@users.noreply.github.com>
2020-01-13 20:42:07 +0100

    Made small keyframe template and some js
    Made a small bit of the keyframes template and done it so javascript automatically adds the name of the keyframes for every element

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let timeDelay = -3;
+// Change this to -3 when you don't need the test animation element
+let timeDelay = /*-3*/ 0;
 
 let animationSection = document.querySelector('#animation-section');
 
 for(let i = 1; i <= 100; i++ ) {
     timeDelay += 3;
-
-    
 
     let newDiv = document.createElement('div');
     newDiv.innerHTML = 'Happy Birthday<br>Nikku';
@@ -14,6 +13,7 @@ for(let i = 1; i <= 100; i++ ) {
     animationSection.appendChild(newDiv);
     
     newDiv.style.animationDelay = `${timeDelay}s`;
+    newDiv.style.animationName = `animation-element-keyframe-number-${i}`;
     
     /*console.log(timeDelay);
     console.log(i);*/
