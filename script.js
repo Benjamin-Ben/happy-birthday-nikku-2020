a5e02b03ee096ee208438f89e0c1f7733f4e4c9b
Benjamin-Ben <42369601+Benjamin-Ben@users.noreply.github.com>
2020-02-04 19:45:36 +0100

    made the color menu switch work for mint green and light pink

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ document.querySelector('body').addEventListener('keyup', function (e) {
         };
     }
 });
+
+const allShapes = document.querySelectorAll('.shapes_animation_section > div');
+
+
+
+// Light Pink
+document.querySelector('.light_pink').addEventListener('click', function (e) {
+    document.querySelector('#colors-menu-box').style.display = 'none';
+
+    document.querySelector('body').style.backgroundColor = '#ffb6c1';
+
+    for (let i = 0; i < allShapes.length; i++) {
+        allShapes[i].style.backgroundColor = '#e75480';
+    }
+    
+});
+
+
+// Mint Green
+document.querySelector('.mint_green').addEventListener('click', function (e) {
+    document.querySelector('#colors-menu-box').style.display = 'none';
+
+    document.querySelector('body').style.backgroundColor = '#98ff98';
+
+    for (let i = 0; i < allShapes.length; i++) {
+        allShapes[i].style.backgroundColor = 'green';
+    }
+    
+});
